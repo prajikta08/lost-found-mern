b0b0c683bb6de5b0caf506ee563cda6e086927a3
Prajikta Sati <prajiktasati@gmail.com>
2026-06-15 13:41:07 +0530

    Add home route redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -14,6 +19,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        <Route
+  path="/"
+  element={<Navigate to="/login" />}
+/>
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
